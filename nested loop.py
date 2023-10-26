f0637c45d8e519db3d917894d8dc9df575c72dac
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-10-26 21:01:14 +0530

    Add files via upload

---
+++ nested loop.py
@@ -0,0 +1,15 @@
+if 10<20:
+    if 20>10:
+        if 20!=30:
+            if 'a' >'A':
+                print("p")
+            else:
+                print("q")
+        else:
+            print("r")
+    else:
+        print("s")
+else:
+    print("u")
+            
+            
